07d58f7a882d022c4e84b6b14146141c7ec065da
Helena Sánchez <helena.sangar@gmail.com>
2024-03-08 17:07:44 +0100

    new deployed db

--- db/index.js
+++ db/index.js
@@ -2,7 +2,9 @@
 const mongoose = require("mongoose")
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Mamachama"
+  //process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Mamachama"
+    process.env.MONGODB_URI || "mongodb+srv://helena-mamachamadb:9NLgmn2z4CMtb8YA@mamachamadb.viyc9cr.mongodb.net/?retryWrites=true&w=majority&appName=MamachamaDB"
+
 
 mongoose
   .connect(MONGO_URI)
