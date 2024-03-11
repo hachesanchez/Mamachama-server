05a5785ef4f19710153afcbf69ccd0a4ee16f156
Helena Sánchez <helena.sangar@gmail.com>
2024-03-11 22:35:37 +0100

    app.use(cors())

--- config/index.js
+++ config/index.js
@@ -8,11 +8,13 @@ const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000"
 module.exports = (app) => {
   app.set("trust proxy", 1)
 
-  app.use(
-    cors({
-      origin: [FRONTEND_URL]
-    })
-  )
+  // app.use(
+  //   cors({
+  //     origin: [FRONTEND_URL]
+  //   })
+  // )
+
+  app.use(cors())
 
   app.use(logger("dev"))
   app.use(express.json())
